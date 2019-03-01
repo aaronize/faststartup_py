cd8529b9df7be1a12272e0315104fc6de48e9e68
AaronChen <chendai2004@hotmail.com>
2019-03-01 09:04:35 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,3 +2,13 @@
 # coding: utf-8
 
 CUR_ENV=''
+
+if os.environ.has_key("IDCDEPLOY_ENV") and os.environ["IDCDEPLOY_ENV"] == "production":
+    # product environment
+
+    CUR_ENV='PRO'
+
+else:
+    # develop environment
+
+    CUR_ENV='PRE'
